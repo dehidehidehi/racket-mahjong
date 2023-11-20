8a1d242004471104600d0078e8a0658c0ca9b67f
Kevin.COJEAN <kevin.cojean@agysoft.fr>
2023-11-20 22:23:42 +0100

    wip

--- src/main.rkt
+++ src/main.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+;; What is this ?
+;; A simple mahjong hand parser.
+;; To help me learn Racket.
+
 (struct tile ())
 
 (struct tile/honor (honor)
@@ -7,7 +11,8 @@
   #:guard (λ (honor name)
 	    (let ([valid-honors (list 'RD 'GD 'WD 'WW 'SW 'NW 'EW)])
 	      (unless (member honor valid-honors)
-		(error "Invalid honor")
+		;(error "Invalid honor")
+		(println "Invalid honor")
 		(honor)))))
 
 (struct tile/ranked (suit rank)
@@ -16,24 +21,27 @@
 	    (let ([valid-suits (list 'P 'S 'M)])
 	      (and 
 		(unless (member suit valid-suits) 
-		  (error "Invalid suit "))
+		  ;(error "Invalid suit ")
+		(println "Invalid suit")
+		  )
 		(unless (and (number? rank) (>= rank 1) (<= rank 9)) 
-		  (error "Invalid rank"))
+		  ;(error "Invalid rank")
+		(println "Invalid rank")
+		  )
 		(values suit rank)))))
 
+(define (mj/hand tiles-str)
+  (define (parse-symbol t) (λ () (compose string->symbol string (λ (s) (string-ref s 0))) t))
+  (define (parse-tval t) (λ () (compose string->number string (λ (s) (string-ref s 1))) t))
+  (let ([tiles (string-split tiles-str)])
+    (for ([suit (map parse-symbol tiles)]
+	  [rank (map parse-tval tiles)]
+	  [full tiles])
+      (if (rank)
+	(tile/ranked suit rank)
+	(tile/honor full)))))
 
-(define-syntax-rule (mj/hand . varargs)
-  ;(unless (list? tiles-abbr) (error "Invalid param : not a list"))
-  (let ([tiles-abbr (flatten varargs)])
-    ;(for ([suit (map (compose string->symbol (λ s (string-ref s 0)) symbol->string) tiles-abbr)]
-;	  [tval (map (compose (λ s (string-ref s 1)) symbol->string) tiles-abbr)])
-      ;(match (cons suit tval)
-      (match (tiles-abbr)
-	;['(_ number?) (tile/ranked suit tval)]
-	[else "unknown!"])))
-
-
-(define ok (mj/hand 'P1))
+(define ok (mj/hand "P1 P1"))
 (ok)
 ;(define ok (mj/hand P1 P1 P1 M1 M2 M3 S5 S6 S7 WW WW))
 
