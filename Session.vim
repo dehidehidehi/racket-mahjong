e8ee76d1e4c95e5af8d058a1962ad688d2a60678
Kevin.COJEAN <kevin.cojean@agysoft.fr>
2023-12-18 21:42:39 +0100

    detects triplets and sequences well

--- Session.vim
+++ Session.vim
@@ -1,25 +1,18 @@
 let SessionLoad = 1
 if &cp | set nocp | endif
-let s:so_save = &so | let s:siso_save = &siso | set so=0 siso=0
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
 let v:this_session=expand("<sfile>:p")
 silent only
 silent tabonly
-cd ~/Development/mahjong
+cd ~/Documents/Development/learning-racket/racket-mahjong
 if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
 set shortmess=aoO
 argglobal
 %argdel
-$argadd src/main.rkt
-edit src/main.rkt
-set splitbelow splitright
-set nosplitbelow
-wincmd t
-set winminheight=0
-set winheight=1
-set winminwidth=0
-set winwidth=1
+$argadd main.rkt
+edit main.rkt
 argglobal
 setlocal fdm=manual
 setlocal fde=0
@@ -30,25 +23,25 @@ setlocal fml=1
 setlocal fdn=20
 setlocal fen
 silent! normal! zE
-let s:l = 259 - ((20 * winheight(0) + 20) / 41)
+let &fdl = &fdl
+let s:l = 139 - ((21 * winheight(0) + 23) / 46)
 if s:l < 1 | let s:l = 1 | endif
-exe s:l
+keepjumps exe s:l
 normal! zt
-259
+keepjumps 139
 normal! 03|
 tabnext 1
-badd +0 src/main.rkt
+badd +0 main.rkt
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0
   silent exe 'bwipe ' . s:wipebuf
 endif
 unlet! s:wipebuf
 set winheight=1 winwidth=20 shortmess=filnxtToOSc
-set winminheight=1 winminwidth=1
 let s:sx = expand("<sfile>:p:r")."x.vim"
-if file_readable(s:sx)
+if filereadable(s:sx)
   exe "source " . fnameescape(s:sx)
 endif
-let &so = s:so_save | let &siso = s:siso_save
+let &g:so = s:so_save | let &g:siso = s:siso_save
 nohlsearch
 doautoall SessionLoadPost
 unlet SessionLoad
--- main.rkt
+++ main.rkt
@@ -2,18 +2,23 @@
 #lang debug racket
 (require debug/repl)
 
+
 ; Ideas for snippets
 ; module+ test (...)
 ; htdf template 
 ; htdd enum template 
 ; htdd composite data template
 
+
 (module+ test
   (require rackunit))
 
+
 (module* templates #f
 
+
   (define t/... (empty))
+
 
   ;; Suit is one of:
   ;; - M (manzu)
@@ -27,12 +32,14 @@
       [(eq? suit 'P) (t/...)]
       [else (t/...)]))
 
+
   ;; Rank is Integer
   ;; interpr. the rank of a tile, restricted to [-1, 9], -1 corresponds to a aka 5 and should equal? 5.
   (define (template-process-rank rank)
     (cond 
       [(or (= rank 5) (= -1 rank)) (t/... rank)]
       [else (t/... rank)]))
+
 
   ;; s/tile is (make-s/tile Symbol Integer)
   ;; interp. a sequence tile, with a suit and a rank.
@@ -49,6 +56,7 @@
 			  [else (t/... rank)])])
       (t/... handle-suit handle-rank)))
 
+
   ;; Honor is one of:
   ;; - WW (west wind)
   ;; - NW (north wind)
@@ -60,6 +68,7 @@
   ;; interp. a tile which is an honor.
   (define (template-process-h/tile honor)
     (cond
+      [(not (valid-honor? honor)) #f]
       [(eq? honor 'WW) (t/...)]
       [(eq? honor 'NW) (t/...)]
       [(eq? honor 'EW) (t/...)]
@@ -67,7 +76,11 @@
       [(eq? honor 'GD) (t/...)]
       [(eq? honor 'RD) (t/...)]
       [(eq? honor 'WD) (t/...)]
+      [(eq? honor 'GD/aka) (t/...)]
+      [(eq? honor 'RD/aka) (t/...)]
+      [(eq? honor 'WD/aka) (t/...)]
       [else (t/...)]))
+
 
   ;; Hand is (make-hand (ListOf (Union s/tile Symbol)))
   ;; interp. a hand of 0 to 14 tiles, which are either s/tile or honor tiles.
@@ -96,20 +109,24 @@
 	 [(eq? tile 'GD) (t/...)]
 	 [(eq? tile 'RD) (t/...)]
 	 [(eq? tile 'WD) (t/...)]
+	 [(eq? tile 'GD/aka) (t/...)]
+	 [(eq? tile 'RD/aka) (t/...)]
+	 [(eq? tile 'WD/aka) (t/...)]
 	 [else (t/...)]))
       )
     )
 
   (define (template-hand-complete? hand) (t/... hand))
-
   )
+
 
 ;; Predicate (ListOf Any) -> (Union #f (ListOf Any))
 ;; Utility function.
 ;; Reduces a list applying a predicate which has a fixed arity of 1.
 ;; Returns #f if any application of the predicate returns #f.
-(define (all? bipred lst)
-  (if (andmap bipred lst) lst #f))
+(define (all? pred lst)
+  (if (andmap pred lst) lst #f))
+
 
 ;; Predicate (ListOf Any) -> (Union #f (ListOf Any))
 ;; Utility function.
@@ -118,50 +135,137 @@
 (define (all/bi? bipred lst)
   (if (andmap (λ (x) (bipred x (first lst))) (rest lst)) lst #f))
 
-;; (ListOf Any) Any Any -> (ListOf Any)
+
+;; Any Any (ListOf Any) -> (ListOf Any)
 ;; Replaces a value in a list by another value when the the equal? procedure returns #t
 (define (replace v1 v2 lst)
   (map (λ (e) (if (equal? v1 e) v2 e)) lst))
 (module+ test
   (check-equal? (replace 1 2 (list 2 1 2)) (list 2 2 2)))
 
-(list 'WW 'NW 'EW 'SW 'GD 'RD 'WD)
+
+(list 'WW 'NW 'EW 'SW 'GD 'RD 'WD 'RD/aka 'GD/aka 'WD/aka)
 (list 'CHINIISOU)
 (define-struct s/tile (suit rank) #:transparent)
 (define-struct hand (tiles) #:transparent)
 
+
+(define (s/tile-same-suit? tiles) 
+  (if (all/bi? equal? (map s/tile-suit tiles)) tiles #f))
+(module+ test
+  (define (m n) (make-s/tile 'M n))
+  (define (s n) (make-s/tile 'S n))
+  (check-equal? (s/tile-same-suit? (list (m 1) (m 2) (m 3))) (list (m 1) (m 2) (m 3)))
+  (check-false (s/tile-same-suit? (list (m 1) (s 2) (m 3))) #f))
+
+
+(define (s/tile-same-rank? tiles) 
+  (if (all/bi? = (map s/tile-rank tiles)) tiles #f))
+(module+ test
+  (check-equal? (s/tile-same-rank? (list (m 1) (m 1) (m 1))) (list (m 1) (m 1) (m 1)))
+  (check-false (s/tile-same-rank? (list (m 1) (m 2) (m 1)))))
+
+
+;; Symbol -> (Union Symbol #f)
+;; Returns the honor if the symbol is considered a valid honor, else #f.
+(define (valid-honor? honor)
+    (cond
+      [(eq? honor 'WW) honor]
+      [(eq? honor 'NW) honor]
+      [(eq? honor 'EW) honor]
+      [(eq? honor 'SW) honor]
+      [(eq? honor 'GD) honor]
+      [(eq? honor 'RD) honor]
+      [(eq? honor 'WD) honor]
+      [(eq? honor 'GD/aka) honor]
+      [(eq? honor 'RD/aka) honor]
+      [(eq? honor 'WD/aka) honor]
+      [else #f]))
+(module+ test
+  (check-eq? (valid-honor? 'WW) 'WW)
+  (check-eq? (valid-honor? 'NW) 'NW)
+  (check-eq? (valid-honor? 'EW) 'EW)
+  (check-eq? (valid-honor? 'SW) 'SW)
+  (check-eq? (valid-honor? 'GD) 'GD)
+  (check-eq? (valid-honor? 'RD) 'RD)
+  (check-eq? (valid-honor? 'WD) 'WD)
+  (check-eq? (valid-honor? 'GD/aka) 'GD/aka)
+  (check-eq? (valid-honor? 'RD/aka) 'RD/aka)
+  (check-eq? (valid-honor? 'WD/aka) 'WD/aka))
+
+
+(define (all-valid-honors? honors)
+  (if (andmap valid-honor? honors) honors #f))
+(module+ test
+  (check-equal? (all-valid-honors? (list 'NW 'WW 'GD/aka)) (list 'NW 'WW 'GD/aka))
+  (check-false (all-valid-honors? (list 'NW 'WW 'GD/aka 'WHAT?))))
+	
+
+;; ListOf s/tiles -> (Union (ListOf s/tiles) #f)
+;; If all s/tile ranks are equal then returns the list of tiles.
+;; This takes into account red fives.
+(define (all-s/tile-ranks=? tiles)  ; ListOf s/tile
+  (define ranks (map s/tile-rank tiles))
+  (if (and (s/tile-same-suit? tiles) 
+       (or (s/tile-same-rank? tiles)
+	   (equal? (list 5 5 5) (replace -1 5 ranks))))
+    tiles
+    #f))
+(module+ test 
+  (check-equal? (all-s/tile-ranks=? (list (m 5) (m 5) (m 5))) (list (m 5) (m 5) (m 5)))
+  (check-equal? (all-s/tile-ranks=? (list (m -1) (m 5) (m 5))) (list (m -1) (m 5) (m 5)))
+  (check-false (all-s/tile-ranks=? (list (m 1) (m 5) (m 5)))))
+
+
+;; ListOf Symbol -> (Union (ListOf Symbol) #f)
+;; If all honor tiles are equal then returns the list of honors.
+;; This takes into account red dragons.
+(define (all-honors-equal? honors)
+  (cond
+    [(not (all-valid-honors? honors)) #f]
+    [(all/bi? eq? honors) honors]
+    [(and (member 'GD/aka honors) (all/bi? eq? (replace 'GD/aka 'GD honors))) honors]
+    [(and (member 'RD/aka honors) (all/bi? eq? (replace 'RD/aka 'RD honors))) honors]
+    [(and (member 'WD/aka honors) (all/bi? eq? (replace 'WD/aka 'WD honors))) honors]
+    [else #f]))
+(module+ test 
+  (check-equal? (all-honors-equal? (list 'WW 'WW 'WW)) (list 'WW 'WW 'WW))
+  (check-equal? (all-honors-equal? (list 'GD 'GD 'GD)) (list 'GD 'GD 'GD))
+  (check-equal? (all-honors-equal? (list 'RD/aka 'RD 'RD)) (list 'RD/aka 'RD 'RD))
+  (check-false (all-honors-equal? (list 'WW 'WW 'NW))))
+
+
 ;; tile ListOf tile -> Union #f ListOf tile
 ;; Returns a completed pair or #f where a pair are 2 identical tiles.
 ;; A red dragon is the same as it's corresponding dragon.
-;; TODO AKA DRAGON
 ;; A red five is the same as it's corresponding five.
 ;; tile ListOf tile -> Union #f ListOf tile
 ;; Returns a completed triplet or #f.
 (define (completes-triplet tile b)
   (define blocked (list* tile b))
   (cond 
-    [(and (= 3 (length blocked))
-	  (cond 
-	    [(and (all? s/tile? blocked)
-		  (all/bi? equal? (map s/tile-suit blocked)) 
-		  (or (let ([ranks (map s/tile-rank blocked)])
-			(all/bi? equal? ranks)
-			(equal? (list 5 5 5) (replace -1 5 ranks)))))]
-	    [else (all/bi? equal? blocked)]))
+    [(not (= 3 (length blocked))) #f]
+    [(and (all? s/tile? blocked) 
+	  (all/bi? equal? (map s/tile-suit blocked))
+	  (all-s/tile-ranks=? blocked))
      blocked]
+    [(and (all-honors-equal? blocked)) blocked]
     [else #f]))
 (module+ test
   (check-false (completes-triplet 'WW (list 'WW)))
   (check-false (completes-triplet 'WW (list 'SW 'WW)))
-  (let ([m2 (make-s/tile 'M 2)]
-	[m5/aka (make-s/tile 'M -1)]
-	[m5 (make-s/tile 'M 5)])
+  (let ([m2 (make-s/tile 'M 2)])
     (check-equal? (completes-triplet m2 (list m2 m2)) (list m2 m2 m2)))
   (check-equal? (completes-triplet 'WW (list 'WW 'WW)) (list 'WW 'WW 'WW))
   (check-equal? (completes-triplet 'GD (list 'GD 'GD)) (list 'GD 'GD 'GD))
   (let ([m5/aka (make-s/tile 'M -1)]
 	[m5 (make-s/tile 'M 5)])
-    (check-equal? (completes-triplet m5/aka (list m5 m5)) (list m5/aka m5 m5))))
+    (check-equal? (completes-triplet m5/aka (list m5 m5)) (list m5/aka m5 m5)))
+  (check-equal? (completes-triplet 'GD/aka (list 'GD 'GD)) (list 'GD/aka 'GD 'GD))
+  (check-equal? (completes-triplet 'RD/aka (list 'RD 'RD)) (list 'RD/aka 'RD 'RD))
+  (check-equal? (completes-triplet 'WD/aka (list 'WD 'WD)) (list 'WD/aka 'WD 'WD))
+  (check-equal? (completes-triplet 'WD (list 'WD 'WD/aka)) (list 'WD 'WD 'WD/aka)))
+
 
 ;; tile ListOf tile -> Union #f ListOf tile
 ;; Given two tiles and one more tile, returns a completed sequence or #f.
@@ -204,6 +308,11 @@
   (check-equal? (completes-sequence m5/aka (list m7 m6)) (list m5/aka m7 m6)))
 
 
+
+;; TODO completes pair 
+
+;; TODO compose hand
+
 ;; hand blocks -> ListOf ListOf tile
 ;; divides the hand into sequence, pair, or honor blocks.
 ;(define (block-hand hand [blocks empty])
@@ -227,13 +336,16 @@
 ;			   [(eq? suit 'P) suit]
 ;			   [else #f])
 ;			 (t/... rank)))]
-;	  [(eq? tile 'WW) (t/...)]
-;	  [(eq? tile 'NW) (t/...)]
-;	  [(eq? tile 'EW) (t/...)]
-;	  [(eq? tile 'SW) (t/...)]
-;	  [(eq? tile 'GD) (t/...)]
-;	  [(eq? tile 'RD) (t/...)]
-;	  [(eq? tile 'WD) (t/...)]
+	; [(eq? honor 'WW) (t/...)]
+	; [(eq? honor 'NW) (t/...)]
+	; [(eq? honor 'EW) (t/...)]
+	; [(eq? honor 'SW) (t/...)]
+	; [(eq? honor 'GD) (t/...)]
+	; [(eq? honor 'RD) (t/...)]
+	; [(eq? honor 'WD) (t/...)]
+	; [(eq? honor 'GD/aka) (t/...)]
+	; [(eq? honor 'RD/aka) (t/...)]
+	; [(eq? honor 'WD/aka) (t/...)]
 ;	  [else (t/...)]]))])))
 ;
 ;(module+ test
@@ -276,3 +388,4 @@
 ;
 ;
 ;
+
